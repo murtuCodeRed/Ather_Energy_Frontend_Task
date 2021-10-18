85f3a0c4a74ea741b2e3001b80f875a29cc2b006
murtuCodeRed <murtazay619@gmail.com>
2021-10-18 21:55:56 +0400

    Automated Code

--- 2048_console.py
+++ 2048_console.py
@@ -56,17 +56,8 @@ def printmat(mat):
         print("\n")
     return 0
 
-def check(mat):
-    for i in range(0,size):
-        for j in range(0,size):
-            if mat[i][j]==".":
-                continue
-            if mat[i][j]==2048:
-                return 1
-    return 0
-
 def game():
-    print("\n\t\t\t\t\t2048")
+    print("\n\t\t<<<<  2048 GAME >>>>")
 
     mat = []
     for i in range(0,size):
@@ -75,31 +66,33 @@ def game():
             a.append(".")
         mat.append(list(a))
     
-    mat[0][0]="2"
+    mat[0][0]="1024"
+    mat[0][1]="1024"
 
     direction = {'L': 0, 'B': 1, 'R': 2, 'T': 3, 'X': 4}
 
     printmat(mat)
     loseStatus = 0
-    winStatus=0
+    keys=["R", "r", "L", "l", "T", "t", "B", "b"]
+
     move.score = 0 # Score of the user
     while True:
-        tmp = input("\nTo continue, Press L for left, R for right, T for top, B for bottom or\nPress X to end the game.\n")
-        if tmp in ["R", "r", "L", "l", "T", "t", "B", "b", "X", "x"]:
+        t=random.randint(0, len(keys)-1)
+        tmp=keys[t]
+        if tmp in keys:
             dir = direction[tmp.upper()]
             if dir == 4:
+                print("\nGame Exiting..........")
+                print("\nFinal score: " + str(move.score))
+                break
+            elif move.score>=2048:
+                print("\nYou have won!!!!")
                 print("\nFinal score: " + str(move.score))
                 break
             else:
                 mat = move(mat, dir)
                 mat, loseStatus = addNumber(mat)
-                winStatus=check(mat)
                 printmat(mat)
-                
-                if winStatus:
-                    print("\nYou have won!!!!")
-                    print ("Final score: " + str(move.score))
-                    break          
                 if loseStatus:
                     print ("\nGame Over")
                     print ("Final score: " + str(move.score))
